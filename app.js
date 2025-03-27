c47db2074d2157d479e84202d7d1538f7c00db62
Felipe Víctor <felipe@oitsys.com>
2025-03-27 14:18:58 -0600

    feat: Add fetching data from OpenAI code and add messages with emojis to the container

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Importar dependencias
+// Importar dependencias.
 import express from "express";
 import dotenv from "dotenv";
 import OpenAI from "openai";
@@ -7,33 +7,33 @@ const systemPrompt1 = "Eres un traductor profesional de multiples idiomas.";
 const systemPrompt2 = "Solo puedes responder con una traducción directa del texto que el usuario te envíe."
                     + "Cualquier otra respuesta o conversación está prohibida";
 
-// Cargar configuración del Environment
+// Cargar configuración del Environment.
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const OPENAI_API_KEY =  process.env.OPENAI_API_KEY;
 
-// Crear instancia de openai y pasar el api key
+// Crear instancia de OpenAI y pasar el API Key.
 const openai = new OpenAI({
    apiKey: OPENAI_API_KEY
 });
 
-// Cargar Express
+// Cargar Express.
 const app = express();
 
-// Middleware para procesar lo que el usuario envía en el body de POST
+// Middleware para procesar lo que el usuario envía en el body a través de POST.
 app.use(express.json()); // json
 app.use(express.urlencoded({extended: true})); // x-www-form-urlencoded
 
-// Servir frontend
+// Servir frontend.
 app.use("/", express.static("public"));
 
-// Endpoint
+// Endpoint para OpenAI.
 app.post('/api/translate', async (req, res) => {
 
    const {userText, targetLang} = req.body;
    const userPrompt = `Traduce el siguiente texto a ${targetLang}: ${userText}`;
 
-   //Llamar al LLM o modelo de openai
+   //Llamar al LLM o modelo de OpenAI.
    try {
       const completion = await openai.chat.completions.create({
          model: "gpt-3.5-turbo",
@@ -50,14 +50,11 @@ app.post('/api/translate', async (req, res) => {
       return res.status(200).json({translatedText});
 
    } catch(e) {
-      return res.status(e.status).json({
-         status: e.status,
-         message: e.error.message
-      });
+      return res.status(500).json({translatedText: 'No puedo traducir tu texto en este momento.'});
    }
 });
 
-// servir el backend
+// Servir el Backend.
 app.listen(PORT, () => {
    console.log(`Servidor corriendo correctamente en http://localhost:${PORT}`);
 });
---
+++ public/assets/js/main.js
@@ -0,0 +1,73 @@
+const htmlInputText = document.querySelector('#inputText');
+const htmlMessagesContainer = document.querySelector('.chat__messages');
+const htmlTranslateButton = document.querySelector('#translateButton');
+
+const botMessageIcons = {
+   'aleman': ' 🇩🇪',
+   'español de mexico': ' 🇲🇽',
+   'frances': ' 🇫🇷',
+   'ingles americano': ' 🇺🇸',
+   'italiano': ' 🇮🇹',
+   'portugues': ' 🇵🇹',
+   '400': '⚠️',
+   '500': '🛑'
+};
+
+
+const createHtmlDiv = (role, text, icon) => { // role = 'user' | 'bot'
+
+   let htmlIcon;
+   const htmlDiv = document.createElement('div');
+
+   htmlDiv.textContent = text;
+   htmlDiv.className = 'chat__message chat__message--' + role;
+
+   if (role === 'bot') {
+      htmlIcon = document.createElement('div');
+      htmlIcon.className = 'message__flag';
+      htmlIcon.textContent = botMessageIcons[icon];
+      htmlDiv.appendChild(htmlIcon);
+   }
+
+   htmlMessagesContainer.appendChild(htmlDiv);
+   htmlMessagesContainer.scrollTop = htmlMessagesContainer.scrollHeight;
+
+};
+
+
+htmlTranslateButton.addEventListener('click', async () => {
+
+   const targetLang = document.querySelector('#targetLang').value;
+   const textToTranslate = htmlInputText.value.trim();
+
+   if(!textToTranslate) {
+      createHtmlDiv('bot','🤖 Escribe una palabra o frase para traducirla.', '400');
+      return false;
+   }
+
+   // Colocar el mensaje del usuario a la caja de mensajes.
+   createHtmlDiv('user', '💬 ' + textToTranslate);
+
+   // Petición Ajax al Backend.
+   try {
+      const response = await fetch('/api/translate', {
+         method: 'POST',
+         headers: {'Content-Type': 'application/json'},
+         body: JSON.stringify({
+            textToTranslate,
+            targetLang
+         })
+      });
+
+      const data = await response.json();
+      const icon = response.status === 200 ? targetLang : '500';
+      createHtmlDiv('bot', '🤖 ' + data.translatedText, icon);
+
+   } catch(e) {
+      console.log('Error: ', e);
+   }
+
+   // Borrar el contenido del input del mensaje.
+   htmlInputText.value = '';
+
+});
